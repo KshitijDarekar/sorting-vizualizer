5564a2a2c2acbfc2d65b87baf409368981b3e05a
Kshitij Darekar <kshitijdarekar13@gmail.com>
2021-05-20 16:51:18 +0530

    improve ui and fixes

--- js_files/quick.js
+++ js_files/quick.js
@@ -5,31 +5,24 @@
 //     let i=low;
 //     let j=high;
 
+//     // if(i>j){
+//     //     return;
+//     // }
 
 //     while(i<j){
-//         do{
-//             i++;
-//                 // To pause the execution of code for 600 milliseconds
-//                 await new Promise((resolve) =>
-//                 setTimeout(() => {
-//                 resolve();
-//                 }, 100)
-//             );
+
+        
+//         while(Number(bars[i].childNodes[0].innerHTML)  <= pivot){
 //             console.log( "Child Node = "+(bars[i].childNodes[0].innerHTML + " i = " + i)) ;
 
-//         }while( Number(bars[i].childNodes[0].innerHTML)  < pivot);
-    
-//         do{
-//             j--;
-//                     // To pause the execution of code for 600 milliseconds
-//                     await new Promise((resolve) =>
-//                     setTimeout(() => {
-//                     resolve();
-//                     }, 100)
-//                 );
-                
+//             i++;        
+//         }
 
-//         }while( Number(bars[j].childNodes[0].innerHTML) >  pivot);
+//         while(Number(bars[j].childNodes[0].innerHTML) >  pivot){
+//             j--;
+
+//         }
+    
     
         
 //     // Changing the color of elements to be swapped 
@@ -78,20 +71,30 @@
 // };
 //take input from arr_sz to change number of bars
 
-const button4 = document.getElementById("Button4");
-button4.addEventListener("click", quickSort.bind(null,0,num-1));
 
+//~~ Kshitij Darekar
+
+const button4 = document.getElementById("Button4");
+
+button4.addEventListener("click", quickSortCall.bind(null));
+
+function quickSortCall(){
+    num = Number(document.getElementById("arr_sz").value);
+    quickSort(0,num-1);
+}
 
 
 async function partition(start, end){
     let bars = document.querySelectorAll(".bar");//. 
     let i= start + 1;
     let piv = Number(bars[start].childNodes[0].innerHTML) ;            //make the first element as pivot element.
+    bars[start].style.backgroundColor="#6b5b95";
     for(let j =start + 1; j <= end ; j++ )  {
     /*rearrange the array by putting elements which are less than pivot
        on one side and which are greater that on other. */
 
        
+
        // To pause the execution of code for 600 milliseconds
                     await new Promise((resolve) =>
                     setTimeout(() => {
@@ -99,19 +102,26 @@ async function partition(start, end){
                     }, 100)
                 );
 
+                console.log( "j="+j+ "bars[j]= "+Number(bars[j].childNodes[0].innerHTML)+ " pivot= "+ piv)
           if ( Number(bars[j].childNodes[0].innerHTML) < piv) {
+
+                if(j!=i){
+
+                    
+
+              
                  //swap (bars[ i ],bars[ j ]);
                                     
                 // Changing the color of elements to be swapped 
-                bars[i].style.backgroundColor = " rgb(49, 226, 13)"; //#6b5b95 
-                bars[j].style.backgroundColor = " rgb(49, 226, 13)"; //rgb(0, 183, 255)
+                bars[i].style.backgroundColor = "#ffd32a"; //#6b5b95 
+                bars[j].style.backgroundColor = "#ff4242"; //rgb(0, 183, 255)
 
                 // To pause the execution of code for 600 milliseconds
                 await new Promise((resolve) =>
                 setTimeout(() => {
                 resolve();
                 }, 200)
-            );
+                );
 
                 temp1 = bars[j].style.height; 
                 temp2 = bars[j].childNodes[0].innerText; 
@@ -120,20 +130,24 @@ async function partition(start, end){
                 bars[j].childNodes[0].innerText = bars[i].childNodes[0].innerText; 
                 bars[i].childNodes[0].innerText = temp2;
 
-// To pause the execution of code for 600 milliseconds
-await new Promise((resolve) =>
-setTimeout(() => {
-resolve();
-}, 200)
-);
+                // To pause the execution of code for 600 milliseconds
+                await new Promise((resolve) =>
+                setTimeout(() => {
+                resolve();
+                }, 200)
+                );
 
-                
-    //  // Changing the color to the previous one 
-     bars[i].style.backgroundColor = "rgb(0, 183, 255)"; //#6b5b95 
-     bars[j].style.backgroundColor = "rgb(0, 183, 255)"; //rgb(0, 183, 255)            
-            i += 1;
-        }
-   }
+                            
+                //  // Changing the color to the previous one 
+                bars[i].style.backgroundColor = "rgb(0, 183, 255)"; //#6b5b95 
+                bars[j].style.backgroundColor = "rgb(0, 183, 255)"; //rgb(0, 183, 255)  
+                }
+
+                bars[j].style.backgroundColor = "rgb(0, 183, 255)"; //#6b5b95 
+
+                        i += 1;
+                    }
+            }
                     // To pause the execution of code for 600 milliseconds
                     await new Promise((resolve) =>
                     setTimeout(() => {
@@ -141,14 +155,14 @@ resolve();
                     }, 200)
                 );
 
-   //put the pivot element in its proper place.
-    temp1 = bars[start].style.height; 
-    temp2 = bars[start].childNodes[0].innerText; 
-    bars[start].style.height = bars[i-1].style.height; 
-    bars[i-1].style.height = temp1; 
-    bars[start].childNodes[0].innerText = bars[i-1].childNodes[0].innerText; 
-    bars[i-1].childNodes[0].innerText = temp2;
-                    
+        //put the pivot element in its proper place.
+            temp1 = bars[start].style.height; 
+            temp2 = bars[start].childNodes[0].innerText; 
+            bars[start].style.height = bars[i-1].style.height; 
+            bars[i-1].style.height = temp1; 
+            bars[start].childNodes[0].innerText = bars[i-1].childNodes[0].innerText; 
+            bars[i-1].childNodes[0].innerText = temp2;
+                            
 
                         // To pause the execution of code for 600 milliseconds
                         await new Promise((resolve) =>
@@ -164,7 +178,7 @@ resolve();
 }
 
 
-async function quickSort(low, high, delay = 100) {
+async function quickSort(low, high) {
 let count =0;
 let bars = document.querySelectorAll(".bar");//. 
 
@@ -172,27 +186,27 @@ let bars = document.querySelectorAll(".bar");//.
        
        let j= await partition(low,high);
        if(low < j-1)
-        //       // To pause the execution of code for 600 milliseconds
-        //       await new Promise((resolve) =>
-        //       setTimeout(() => {
-        //       resolve();
-        //       }, 100)
-        //   );
+              // To pause the execution of code for 600 milliseconds
+              await new Promise((resolve) =>
+              setTimeout(() => {
+              resolve();
+              }, 100)
+          );
 
-       await quickSort(low,j);
+       await quickSort(low,j-1);
        for(i=low;i<=j;i++){
-            bars[i].style.backgroundColor="#6b5b95 ";
+            bars[i].style.backgroundColor="#13CE66";
        }
        //console.log(++count);
-        //       // To pause the execution of code for 600 milliseconds
-        //       await new Promise((resolve) =>
-        //       setTimeout(() => {
-        //       resolve();
-        //       }, 100)
-        //   );
+              // To pause the execution of code for 600 milliseconds
+              await new Promise((resolve) =>
+              setTimeout(() => {
+              resolve();
+              }, 100)
+          );
        await quickSort(j+1,high);
        for(i=j+1;i<=high;i++){
-        bars[i].style.backgroundColor="#6b5b95 ";
+        bars[i].style.backgroundColor="#13CE66";
    }
        //console.log(++count);
    }
